04733c4905879fea5872560e5dd3fe1a9f87f5e2
Dominick Peluso <peluso.dominick@gmail.com>
2014-09-18 16:14:31 -0400

    Cookie seems to be working

--- js/currency.jquery.js
+++ js/currency.jquery.js
@@ -44,11 +44,69 @@ $( document ).ready(function() {
         var newly_selected_currency_code = $(this).val();
         var old_currency_rate = 'USD';
 
+
+        // Read/set cookies
+        var old_currency_code = set_currency_cookie(newly_selected_currency_code);
+
+        if(old_currency_code.length){
+            console.log('old code was '+old_currency_code);
+        } else {
+            console.log('old code was null, USD set');
+            old_currency_code = "USD";
+        }
+
+        // Set some data attributes
+        $(this).attr('data-currency-old', old_currency_code);
+        $(this).attr('data-currency-new', newly_selected_currency_code);
+
         var rate_response = get_currency_rate(old_currency_rate, newly_selected_currency_code);
 
 
 
     });
+
+    // Make and handle cookies
+    function set_currency_cookie(new_currency_type){
+
+        // Stock functions
+        function createCookie(name,value,days) {
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime()+(days*24*60*60*1000));
+                var expires = "; expires="+date.toGMTString();
+            }
+            else var expires = "";
+            document.cookie = name+"="+value+expires+"; path=/";
+        }
+
+        function readCookie(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        }
+
+        function eraseCookie(name) {
+            createCookie(name,"",-1);
+        }
+
+        // Action
+        var cookie_name = 'VeracoreCurrencyDefault';
+        // Read old cookie value
+        var old_currency_type = readCookie(cookie_name);
+        console.log('OLD CURRENCY TYPE');
+        console.log(old_currency_type);
+        // Delete a cookie if one exists
+        eraseCookie(cookie_name);
+        // Create new one
+        createCookie(cookie_name, new_currency_type, 7);
+
+        return old_currency_type;
+    }
 
     // Replace all values on the page
     function change_currency_value(rate_response){
@@ -57,10 +115,29 @@ $( document ).ready(function() {
 
         console.log(rate_response);
 
+
         $(watch_list).each( function(index, node) {
-            $(node).html('gotcha');
-            console.log(node);
-            console.log(node.context.nodeName);
+
+            // Set some data attributes
+            $(node).attr('data-currency-old', rate_response.from);
+            $(node).attr('data-currency-new', rate_response.to);
+
+            var element_with_price_text = $(node).text();
+
+            var old_price = Number( element_with_price_text.replace(/[^0-9\.]+/g,""));
+
+            // Do some math
+            function roundToTwo(num) {
+                return +(Math.round(num + "e+2")  + "e-2");
+            }
+
+            var new_price_long = (old_price * rate_response.rate);
+
+            var new_price = roundToTwo(new_price_long);
+
+            // Write the new price out
+            $(node).text(new_price);
+
         });
 
     }
