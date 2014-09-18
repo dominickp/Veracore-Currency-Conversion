da021e0ae4549194a8585720d432191aa4d8e6e9
Dominick Peluso <peluso.dominick@gmail.com>
2014-09-18 14:53:39 -0400

    Working to some extent

--- js/currency.jquery.js
+++ js/currency.jquery.js
@@ -2,21 +2,33 @@ $( document ).ready(function() {
 
     // Currencies
     var currencies = {
-        "USD" : '&#xf155; US Dollar',
-        "GBP" : "&#xf154; British Pound",
-        "EUR" : "&#xf153; Euro"
+        "USD" : {
+            "name"  : "US Dollar",
+            "fa"    : "&#xf155;",
+            "symbol": "$"
+        },
+        "GBP" : {
+            "name"  : "British Pound",
+            "fa"    : "&#xf154;",
+            "symbol": "$"
+        },
+        "EUR" : {
+            "name"  : "Euro",
+            "fa"    : "&#xf153;",
+            "symbol": "€"
+        }
     }
 
     // Build currency converter
     function build_currency_selector(){
 
-        var select_begin = '<select class="form-control">';
+        var select_begin = '<select id="select_currency" class="form-control">';
         var option_list = '';
 
         // Building options list
-        $.each(currencies, function(index, value) {
-            option_list = option_list + '<option>' + value + '</option>';
-            console.log(1);
+        $.each(currencies, function(code, currency) {
+            option_list = option_list + '<option value="' + code + '">' + currency["fa"] + ' ' + currency["name"] + '</option>';
+            //console.log(1);
         });
 
         var select_end = '</select>';
@@ -26,7 +38,72 @@ $( document ).ready(function() {
 
     }
 
+    // When dropdown selection changes
+
+    $( "#currency_converter" ).delegate( "#select_currency", "change", function() {
+
+        var newly_selected_currency_code = $(this).val();
+
+        get_currency_rate('USD', newly_selected_currency_code);
+
+        console.log($(this).getWatchList());
+
+    });
+
+    // Get live currency rate
+    function get_currency_rate(old_currency, new_currency){
+        var rate_exchange_url = 'http://rate-exchange.appspot.com/currency?';
+        var quantity = 1;
+        var rate_string = 'from='+old_currency+'&to='+new_currency+'&q='+quantity;
+
+        // Get the rate/value
+        $.ajax({
+            url: rate_exchange_url+rate_string,
+            jsonp: "callback",
+            dataType: "jsonp",
+            data: {
+                q: "to, rate, from, v",
+                format: "json"
+            },
+
+            // work with the response
+            success: function( response ) {
+                console.log( response ); // server response
+            }
+        });
+    }
+
+
+
     // Initialize
     build_currency_selector();
 
+    //get_currency_rate('USD', 'EUR');
+
+
+
+
+
 });
+
+
+(function( $ ) {
+
+    var watch_list = new Array();
+
+    $.fn.currencyWatch = function() {
+
+        var test = 'test';
+
+        var to_watch = $(this).toArray();
+
+        watch_list.push(to_watch);
+        //console.log('pushed one');
+
+    };
+
+    $.fn.getWatchList = function(){
+        return watch_list;
+    }
+
+}( jQuery ));
